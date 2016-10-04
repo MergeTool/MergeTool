fbae55140ff487876e38ab478a0db25a18e9e6f9
Vasiliy Kerimov <mostlybrainless@parallels.mipt.ru>
2016-10-04 17:31:40 +0300

    simple prototype with confusing GUI

---
+++ prog.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+ 
+using namespace std;
+ 
+int main()
+{
+   int n, status = 1, num = 3, count, c;
+ 
+   cout << "Enter the number of prime numbers to print\n";
+<<<<<<< HEAD
+   cin >> n;
+=======
+   scanf("%d", &n);
+>>>>>>> master
+ 
+   if ( n >= 1 )
+   {
+      cout << "First " << n <<" prime numbers are :-" << endl;
+      cout << 2 << endl;
+   }
+ 
+   for ( count = 2 ; count <=n ;  )
+   {
+      for ( c = 2 ; c <= (int)sqrt(num) ; c++ )
+      {
+         if ( num%c == 0 )
+         {
+            status = 0;
+            break;
+         }
+      }
+      if ( status != 0 )
+      {
+         cout << num << endl;
+         count++;
+      }
+      status = 1;
+      num++;
+   }         
+ 
+   return 0;
+}
