e255f7a4f792b30da76b22fe58cddab7047a9c53
voddan <dgvodopyan@gmail.com>
2016-10-11 15:45:49 +0300

    added a conflicting case into the example

--- prog.cpp
+++ prog.cpp
@@ -30,12 +30,20 @@ int main()
    {
       for ( c = 2 ; c <= (int)sqrt(num) ; c++ )
       {
+<<<<<<< HEAD
          if ( num%c == 0 )
          {
             status = 0;
             break;
+=======
+         if ( num/c == 0 )
+         {
+            status = -1;
+            continue;
+>>>>>>> master
          }
       }
+      printf("hello, world!")
 <<<<<<< HEAD
       status = 0;
       printf("left");
