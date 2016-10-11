e664ba5bd7ae169224aed6716acb7e55fc02e5e3
voddan <dgvodopyan@gmail.com>
2016-10-11 11:29:50 +0300

    refactored the code, streamlined the event loop

--- file.py
+++ file.py
@@ -1,99 +1,156 @@
 import subprocess
+from enum import Enum
 
 
-def compilate():
+class Conflict:
+    def __init__(self, line_number: int, left: str, right: str):
+        self.line_number = line_number
+        self.left = left
+        self.right = right
+        self.select = None
+
+    def select_left(self):
+        self.select = self.left
+
+    def select_right(self):
+        self.select = self.right
+
+    def select_both(self):
+        self.select = self.left + self.right
+
+
+class FileBit:
+    def __init__(self, line_number: int, text: str):
+        self.line_number = line_number
+        self.text = text
+
+
+def combined_file(file_bits: [FileBit], conflicts: [Conflict]) -> str:
+    _file_bits = file_bits[:]
+    _conflicts = conflicts[:]
+    bits = []
+    for i in range(len(file_bits) + len(conflicts)):
+        if i % 2 == 0:
+            bits += file_bits.pop(0).text
+        else:
+            bits += conflicts.pop(0).select
+
+    return "".join(bits)
+
+
+def compilate(file_bits: [FileBit], conflicts: [Conflict]):
     fout = open('code.cpp', 'w')
-    for bit in bits_of_file:
-        for line in bit:
-            fout.write(line)
-
+    fout.write(combined_file(file_bits, conflicts))
     fout.close()
     subprocess.check_output(["g++", "code.cpp"])
 
 
-def conflictServe(list, first):
-    obj = list[first]
-    print("\n left = ")
+def parse_stream_into_conflicts(stream) -> ([FileBit], [Conflict]):
+    class State(Enum):
+        text = 1
+        left = 2
+        right = 3
 
-    for i in range(len(obj[0])):
-        print(obj[0][i][0:len(obj[0][i]) - 1])
+    fileobj_lines = stream.readlines()
+    file_bits = []
+    conflicts = []
 
-    print("\n right = ")
+    state = State.text
+    file_bit = FileBit(0, "")
+    conflict = Conflict(-1, "", "")
 
-    flag1 = False
-    stop = False
+    for index, line in enumerate(fileobj_lines):
+        switch = line[0:3]
 
-    for i in range(len(obj[1])):
-        print(obj[1][i][0:len(obj[1][i]) - 1])
-
-    response = input(
-        "Choose what to leave ('R' = right, 'L' = left, 'B' = both, 'N' = next conflict, 'P' = previous conflict"
-        + " 'C' = compile): \n")
-
-    if (response[0] == 'R') | (response[0] == 'r'):
-        bits_of_file[first * 2 + 1] = obj[1]
-        flag1 = True
-    elif (response[0] == 'L') | (response[0] == 'l'):
-        bits_of_file[first * 2 + 1] = obj[0]
-        flag1 = True
-    elif (response[0] == 'B') | (response[0] == 'b'):
-        new = obj[0]
-        new.expand(obj[1])
-        bits_of_file[first * 2 + 1] = new
-        flag1 = True
-
-    if (response[0] == 'P') | (response[0] == 'p'):
-        flag1 = True
-    elif (response[0] == 'N') | (response[0] == 'n'):
-        flag1 = False
-    elif (response[0] == 'C') | (response[0] == 'c'):
-        compilate()
-        stop = True
-
-    if stop == False:
-        if flag1 == True:
-            if first + 1 != len(list):
-                conflictServe(list, first + 1)
+        if State.text == state:
+            if switch == "<<<":
+                state = State.left
+                file_bits.append(file_bit)
+                file_bit = FileBit(0, "")
+                conflict.line_number = index
             else:
-                conflictServe(list, 0)
+                file_bit.text += line
+
+        elif State.left == state:
+            if switch == "===":
+                state = State.right
+            else:
+                conflict.left += line
+
+        elif State.right == state:
+            if switch == ">>>":
+                state = State.text
+                conflicts.append(conflict)
+                conflict = Conflict(-1, "", "")
+                file_bit.line_number = index + 1
+            else:
+                conflict.right += line
+
         else:
-            if first != 0:
-                conflictServe(list, first - 1)
-            else:
-                conflictServe(list, len(list) - 1)
+            raise ValueError
+
+    file_bits.append(file_bit)
+
+    return file_bits, conflicts
 
 
-conflicts_list = []
-fileobj = open('prog.cpp', 'r', encoding="latin-1")
-fileobj_lines = fileobj.readlines()
-fr = fileobj.read()
-list = [0, 0, 0]
-tuples_list = []
+# changes `conflicts`
+def resolve_conflicts_event_loop(file_bits: [FileBit], conflicts: [Conflict]):
+    unresolved_conflicts = list(range(len(conflicts)))
+    unresolved_conflict_index = 0
+    while True:
+        if not unresolved_conflicts:
+            print("All conflicts have been resolved")
+            break
 
-for index in range(len(fileobj_lines)):
-    if fileobj_lines[index][0:3] == "<<<":
-        list[0] = index
-    elif fileobj_lines[index][0:3] == "===":
-        list[1] = index
-    elif fileobj_lines[index][0:3] == ">>>":
-        list[2] = index
-        tuples_list.append(list)
-        list = [0, 0, 0]
+        unresolved_conflict_index %= len(unresolved_conflicts)
+        conflict = conflicts[unresolved_conflicts[unresolved_conflict_index]]
 
-for tuply in tuples_list:
-    conflict_left = fileobj_lines[tuply[0] + 1:tuply[1]]
-    conflict_right = fileobj_lines[tuply[1] + 1:tuply[2]]
-    conflict = (conflict_left, conflict_right)
-    conflicts_list.append(conflict)
+        print("\n left = ")
+        print(conflict.left)
+        print("\n right = ")
+        print(conflict.right)
 
-fileobj.close()
-bits_of_file = []
-iter = 0
+        response = input(
+            "Choose what to leave ('R' = right, 'L' = left, 'B' = both, "
+            "'N' = next conflict, 'P' = previous conflict 'C' = compile, 'Q' = quit): \n")
 
-for tuply in tuples_list:
-    bits_of_file.append(fileobj_lines[iter:tuply[0]])
-    iter = tuply[2] + 1
-    bits_of_file.append([])
+        switch = response.lower()[0]
 
-bits_of_file.append(fileobj_lines[tuples_list[len(tuples_list) - 1][2] + 1:len(fileobj_lines)])
-conflictServe(conflicts_list, 0)
+        if switch == 'l':
+            conflict.select_left()
+            unresolved_conflicts.pop(unresolved_conflict_index)
+            unresolved_conflict_index += 1
+        elif switch == 'r':
+            conflict.select_right()
+            unresolved_conflicts.pop(unresolved_conflict_index)
+            unresolved_conflict_index += 1
+        elif switch == 'b':
+            conflict.select_both()
+            unresolved_conflicts.pop(unresolved_conflict_index)
+            unresolved_conflict_index += 1
+        elif switch == 'p':
+            unresolved_conflict_index -= 1
+        elif switch == 'n':
+            unresolved_conflict_index += 1
+        elif switch == 'c':
+            compilate(file_bits, conflict)
+        elif switch == 'q':
+            print("Left unresolved %d conflicts" % len(unresolved_conflicts))
+            break
+        else:
+            continue
+
+
+file_stream = open('prog.cpp', 'r', encoding="latin-1")
+
+File_bits, Conflicts = parse_stream_into_conflicts(file_stream)
+
+# conflictServe(conflicts_list, 0)
+
+resolve_conflicts_event_loop(File_bits, Conflicts)
+
+result = combined_file(File_bits, Conflicts)
+print(result)
+
+
