24f8284d40ca90fd81542f86a7551804a347d5ea
voddan <dgvodopyan@gmail.com>
2016-10-31 00:15:07 +0300

    added cpp code samples; set runner to look for `if` statements

--- runer.py
+++ runer.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env python3
+
+# ===- cindex-dump.py - cindex/Python Source Dump -------------*- python -*--===#
+#
+#                     The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+#
+# ===------------------------------------------------------------------------===#
 
 """
 A simple command line tool for dumping a source file using the Clang Index
 Library.
 """
 
-def get_diag_info(diag):
-    return { 'severity' : diag.severity,
-             'location' : diag.location,
-             'spelling' : diag.spelling,
-             'ranges' : diag.ranges,
-             'fixits' : diag.fixits }
 
-def get_cursor_id(cursor, cursor_list = []):
+def get_diag_info(diag):
+    return {'severity': diag.severity,
+            'location': diag.location,
+            'spelling': diag.spelling,
+            'ranges': diag.ranges,
+            'fixits': diag.fixits}
+
+
+def get_cursor_id(cursor, cursor_list=[]):
     if not opts.showIDs:
         return None
 
@@ -20,31 +32,42 @@ def get_cursor_id(cursor, cursor_list = []):
 
     # FIXME: This is really slow. It would be nice if the index API exposed
     # something that let us hash cursors.
-    for i,c in enumerate(cursor_list):
+    for i, c in enumerate(cursor_list):
         if cursor == c:
             return i
     cursor_list.append(cursor)
     return len(cursor_list) - 1
 
+
 def get_info(node, depth=0):
     if opts.maxDepth is not None and depth >= opts.maxDepth:
         children = None
     else:
-        children = [get_info(c, depth+1)
+        children = [get_info(c, depth + 1)
                     for c in node.get_children()]
-    return { 'id' : get_cursor_id(node),
-             'kind' : node.kind,
-             'usr' : node.get_usr(),
-             'spelling' : node.spelling,
-             'location' : node.location,
-             'extent.start' : node.extent.start,
-             'extent.end' : node.extent.end,
-             'is_definition' : node.is_definition(),
-             'definition id' : get_cursor_id(node.get_definition()),
-             'children' : children }
+    return {'id': get_cursor_id(node),
+            'kind': node.kind,
+            'usr': node.get_usr(),
+            'spelling': node.spelling,
+            'location': node.location,
+            'extent.start': node.extent.start,
+            'extent.end': node.extent.end,
+            'is_definition': node.is_definition(),
+            'definition id': get_cursor_id(node.get_definition()),
+            'children': children}
+
+def foreach_child(node, op, depth=0):
+    op(node)
+    if opts.maxDepth is not None and depth >= opts.maxDepth:
+        children = None
+    else:
+        for ch in node.get_children():
+            foreach_child(ch, op, depth + 1)
+
 
 def main():
     from clang.cindex import Index
+    from clang.cindex import CursorKind
     from pprint import pprint
 
     from optparse import OptionParser, OptionGroup
@@ -52,7 +75,7 @@ def main():
     global opts
 
     from clang.cindex import Config
-    Config.set_library_path("/usr/local/opt/llvm/lib/")
+    Config.set_library_file("/usr/local/opt/llvm/lib/libclang.dylib")
 
     parser = OptionParser("usage: %prog [options] {filename} [clang-args*]")
     parser.add_option("", "--show-ids", dest="showIDs",
@@ -68,12 +91,25 @@ def main():
         parser.error('invalid number arguments')
 
     index = Index.create()
-    tu = index.parse(None, args)
-    if not tu:
+    translation_unit = index.parse(None, args)
+    if not translation_unit:
         parser.error("unable to load input")
 
-    pprint(('diags', map(get_diag_info, tu.diagnostics)))
-    # pprint(('nodes', get_info(tu.cursor)))
+    # pprint(('diags', map(get_diag_info, translation_unit.diagnostics)))
+    # pprint(('nodes', get_info(translation_unit.cursor)))
+
+    root = translation_unit.cursor
+
+    def action(n):
+        if n.kind == CursorKind.IF_STMT:
+            print("%s @ %s" % (n.kind, n.location))
+
+    foreach_child(root, action)
+
+    # children = root.get_children()
+    # for ch in children:
+    #     print("%s @ %s" % (ch.kind, ch.location))
+
 
 if __name__ == '__main__':
     main()
---
+++ samples/complicated.cpp
@@ -0,0 +1,45 @@
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+   int n, status = 1, num = 3, count, c;
+
+   cout << "Enter the number of prime numbers to print\n";
+   cin >> n;
+   n += 1;
+   printf("left");
+
+   if ( n >= 1 )
+   {
+      cout << "First " << n <<" prime numbers are :-" << endl;
+      cout << 2 << endl;
+   }
+
+   for ( count = 2 ; count <=n ;  )
+   {
+      for ( c = 2 ; c <= (int)sqrt(num) ; c++ )
+      {
+         if ( num/c == 0 )
+         {
+            status = -1;
+            continue;
+         }
+      }
+      printf("hello, world!");
+      status = 0;
+      printf("right");
+      c = 7;
+      if ( status != 0 )
+      {
+         cout << num << endl;
+         count++;
+      }
+      status = 1;
+      num++;
+   }
+
+   return 0;
+}
