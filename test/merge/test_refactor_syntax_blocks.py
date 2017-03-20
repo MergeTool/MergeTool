fdaf4d44133ec4521ff0c31868acb0a93fe129ce
voddan <dgvodopyan@gmail.com>
2017-03-20 16:24:12 +0300

    Added `TestRefactorMultipleBlocks` tests for code that deals with several blocks at one conflict

--- test/merge/test_refactor_syntax_blocks.py
+++ test/merge/test_refactor_syntax_blocks.py
@@ -502,3 +502,169 @@ class TestRefactorSingleBlock(TestCase):
         file_merge.refactor_syntax_blocks()
         refactored = file_merge.result()
         self.assertEqual(expected, refactored)
+
+
+class TestRefactorMultipleBlocks(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        from merge.external_parser_setup import ExternalParserSetup
+        ExternalParserSetup.setup()
+
+    def test_conflict_at_2_sequential_if_blocks(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                <<<<<<< HEAD
+                        int n = 0;
+                        n += 1;
+                    }
+
+                    if(True) {
+                        int z = 10;
+                =======
+                        int x = 0;
+                        x -= 3;
+                    }
+
+                    if(False) {
+                        int z = 20;
+                >>>>>>> master
+                    }
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int n = 0;
+                        n += 1;
+                    }
+
+                    if(True) {
+                        int z = 10;
+                    }
+                =======
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int x = 0;
+                        x -= 3;
+                    }
+
+                    if(False) {
+                        int z = 20;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_2_nested_if_blocks_ends(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                        if(True) {
+                            int z;
+                <<<<<<< HEAD
+                            z = 10;
+                        }
+                        int n = 0;
+                        n += 1;
+                =======
+                            z = 20;
+                        }
+                        int x = 0;
+                        x -= 3;
+                >>>>>>> master
+                    }
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                <<<<<<< HEAD
+                        if(True) {
+                            int z;
+                            z = 10;
+                        }
+                        int n = 0;
+                        n += 1;
+                =======
+                        if(True) {
+                            int z;
+                            z = 20;
+                        }
+                        int x = 0;
+                        x -= 3;
+                >>>>>>> master
+                    }
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_2_nested_if_blocks_ends_2(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                        if(True) {
+                            int z;
+                <<<<<<< HEAD
+                            z = 10;
+                        }
+                        int n = 0;
+                        n += 1;
+                    }
+                =======
+                            z = 20;
+                        }
+                        int x = 0;
+                        x -= 3;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        printf("Hello!")
+                        if(True) {
+                            int z;
+                            z = 10;
+                        }
+                        int n = 0;
+                        n += 1;
+                    }
+                =======
+                    if(1 > 2) {
+                        printf("Hello!")
+                        if(True) {
+                            int z;
+                            z = 20;
+                        }
+                        int x = 0;
+                        x -= 3;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
