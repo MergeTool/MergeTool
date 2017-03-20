d15f72b941893c419006e446c51f3c1d2ec0aeb2
voddan <dgvodopyan@gmail.com>
2017-03-20 15:57:16 +0300

    Added `TestRefactorSingleBlock` tests for code that deals with a single block, 2 tests with a "raw if" fail

--- test/merge/test_refactor_syntax_blocks.py
+++ test/merge/test_refactor_syntax_blocks.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from pathlib import Path
 from unittest import TestCase
+from unittest import skip
 
 from merge.file_merge import FileMerge
 
@@ -212,3 +213,292 @@ class TestNoRefactor(TestCase):
         refactored = file_merge.result()
 
         self.assertEqual(code, refactored)
+
+
+class TestRefactorSingleBlock(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        from merge.external_parser_setup import ExternalParserSetup
+        ExternalParserSetup.setup()
+
+    def test_conflict_at_if_block_end(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                <<<<<<< HEAD
+                        int n = 0;
+                        n += 1;
+                    }
+                =======
+                        int x = 0;
+                        x -= 3;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int n = 0;
+                        n += 1;
+                    }
+                =======
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int x = 0;
+                        x -= 3;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_if_block_end_2(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2) {
+                        printf("Hello!")
+                <<<<<<< HEAD
+                        int n = 0;
+                        n += 1;
+                    }
+                    printf("World!")
+                =======
+                        int x = 0;
+                        x -= 3;
+                    }
+                    printf("World?")
+                >>>>>>> master
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int n = 0;
+                        n += 1;
+                    }
+                    printf("World!")
+                =======
+                    if(1 > 2) {
+                        printf("Hello!")
+                        int x = 0;
+                        x -= 3;
+                    }
+                    printf("World?")
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_if_block_end_3(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2)
+                <<<<<<< HEAD
+                        { int n = 0; }
+                =======
+                        { int x = 0; }
+                >>>>>>> master
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2)
+                        { int n = 0; }
+                =======
+                    if(1 > 2)
+                        { int x = 0; }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    @skip("Unplanned functionality")
+    def test_conflict_at_raw_if_end(self):
+        code = multiline("""
+                int main() {
+                    if(1 > 2)
+                <<<<<<< HEAD
+                        int n = 0;
+                =======
+                        int x = 0;
+                >>>>>>> master
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2)
+                        int n = 0;
+                =======
+                    if(1 > 2)
+                        int x = 0;
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_if_block_start(self):
+        code = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        int n = 0;
+                        n += 1;
+                =======
+                    if(1 > 2) {
+                        int x = 0;
+                        x -= 3;
+                >>>>>>> master
+                    }
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        int n = 0;
+                        n += 1;
+                    }
+                =======
+                    if(1 > 2) {
+                        int x = 0;
+                        x -= 3;
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_if_block_start_2(self):
+        code = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        int n = 0;
+                        n += 1;
+                =======
+                    if(1 > 2) {
+                        int x = 0;
+                        x -= 3;
+                >>>>>>> master
+                        printf("Hello!")
+                    }
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        int n = 0;
+                        n += 1;
+                        printf("Hello!")
+                    }
+                =======
+                    if(1 > 2) {
+                        int x = 0;
+                        x -= 3;
+                        printf("Hello!")
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    def test_conflict_at_if_block_start_3(self):
+        code = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                =======
+                    if(1 < 2) {
+                >>>>>>> master
+                        printf("Hello!")
+                    }
+                }
+                """)
+
+        expected = multiline("""
+                int main() {
+                <<<<<<< HEAD
+                    if(1 > 2) {
+                        printf("Hello!")
+                    }
+                =======
+                    if(1 < 2) {
+                        printf("Hello!")
+                    }
+                >>>>>>> master
+                }
+                """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
+
+    @skip("Unplanned functionality")
+    def test_conflict_at_raw_if_start(self):
+        code = multiline("""
+                   int main() {
+                   <<<<<<< HEAD
+                       if(1 > 2)
+                   =======
+                       if(1 < 2)
+                   >>>>>>> master
+                           int x = 0;
+                   }
+                   """)
+
+        expected = multiline("""
+                   int main() {
+                   <<<<<<< HEAD
+                       if(1 > 2)
+                           int x = 0;
+                   =======
+                       if(1 < 2)
+                           int x = 0;
+                   >>>>>>> master
+                   }
+                   """)
+
+        file_merge = FileMerge.parse(Path("prog.c"), StringIO(code))
+        file_merge.refactor_syntax_blocks()
+        refactored = file_merge.result()
+        self.assertEqual(expected, refactored)
